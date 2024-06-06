af2196fb5308d90ccb6e2129e47b3e37ec6abe42
Kevin Castillo <kevin.castilloxc@gmail.com>
2024-06-06 04:25:04 -0600

    Creando el paquete modelos para agregar las clases (Records) de las clases a mapear

---
+++ LiterAlura/src/main/java/com/kcastillo/LiterAlura/models/DatosLibros.java
@@ -0,0 +1,12 @@
+package com.kcastillo.LiterAlura.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosLibros(
+        @JsonAlias("results") List<DatosLibro> resultados
+) {
+}
