533fa5c780d4a335ff691ad1468670d4907fe368
Kevin Castillo <kevin.castilloxc@gmail.com>
2024-06-07 02:57:31 -0600

    Cambiando clase DatosLibros por DatosLibrosRecord. Creando clase DatosLibros con un constructor que pase DatosLibrosRecord para obtener solamente el primer lenguaje de cada libro priveniente de la API

---
+++ LiterAlura/src/main/java/com/kcastillo/LiterAlura/models/DatosLibroRecord.java
@@ -0,0 +1,15 @@
+package com.kcastillo.LiterAlura.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true) //To ignore properties not mapping
+public record DatosLibroRecord(
+        @JsonAlias("title") String titulo,
+        @JsonAlias("authors") List<DatosAutor> autor,
+        @JsonAlias("languages") List<String> idiomas,
+        @JsonAlias("download_count") Double numeroDescargas
+        ) {
+}
