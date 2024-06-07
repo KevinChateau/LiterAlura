8bfa5b494359ef05fb8c7f9dc2b7ed131dde1349
Kevin Castillo <kevin.castilloxc@gmail.com>
2024-06-07 01:36:44 -0600

    Creando la clase Operations para la obtención, manipulación y control de los datos obtenidos de la API Gutendex

--- LiterAlura/src/main/java/com/kcastillo/LiterAlura/principal/Principal.java
+++ LiterAlura/src/main/java/com/kcastillo/LiterAlura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.kcastillo.LiterAlura.principal;
 
 import com.kcastillo.LiterAlura.models.DatosLibros;
+import com.kcastillo.LiterAlura.operations.Operations;
 import com.kcastillo.LiterAlura.service.ConsumoAPI;
 import com.kcastillo.LiterAlura.service.ConvierteDatos;
 
@@ -9,57 +10,63 @@ import java.util.Scanner;
 public class Principal {
 
     private Scanner teclado;
-    private final String URL_BASE = "https://gutendex.com/books/";
-    private ConsumoAPI consumoAPI;
-    private ConvierteDatos conversorJsonClass;
+    private Operations operations;
+
 
     public Principal() {
         teclado = new Scanner(System.in);
-        consumoAPI = new ConsumoAPI();
-        conversorJsonClass = new ConvierteDatos();
+        operations = new Operations();
     }
 
     public void showMenu() {
         int exit = 0;
 
-        var json = consumoAPI.obtenerDatos(URL_BASE);
-        var datos = conversorJsonClass.obtenerDatos(json, DatosLibros.class);
 
-        do {
-            System.out.println("***** Bienvenido al MENU principal de la API Gutendex *****");
-            System.out.println("""
-                    Ingrese la operación que desea realizar:
-                                        
-                    1) Buscar libro por título
-                    2) Listar libros registrados
-                    3) Listar autores registrados
-                    4) Listar autores vivos en un determinado año
-                    5) Listar libros por idioma
-                    6) Salir
-                                        
-                    Opción:
-                    """);
-            int opcion = teclado.nextInt();
+        try {
 
 
-            switch (opcion) {
-                case 1:
-                    datos.resultados().forEach(System.out::println);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6: exit = 1;
-                    break;
-            }
+            do {
+                System.out.println("***** Bienvenido al MENU principal de la API Gutendex *****");
+                System.out.println("""
+                        Ingrese la operación que desea realizar:
+                                            
+                        1) Buscar y registrar libro por título
+                        2) Listar libros registrados
+                        3) Listar autores registrados
+                        4) Listar autores vivos en un determinado año
+                        5) Listar libros por idioma
+                        6) Salir
+                                            
+                        Opción:
+                        """);
+
+                int opcion = Integer.valueOf(teclado.nextLine());
 
 
-        } while (exit == 0);
+                switch (opcion) {
+                    case 1:
+                        operations.getAllBooksAPI().forEach(System.out::println);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        exit = 1;
+                        break;
+                    default:
+                        System.out.println("Opción fuera del menú, intente nuevamente");
+                }
 
+
+            } while (exit == 0);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Entrada inválida\n" );
+            nfe.printStackTrace();
+        }
     }
 }
